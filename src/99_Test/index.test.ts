98cf9ac7c98bebed154889e830cb9dd31e1a7cdb
JeongHyeon Kim <jeonghyeon.kim@creatrip.com>
2022-04-11 09:32:10 +0900

    fix: .env filename extension support

--- src/99_Test/index.test.ts
+++ src/99_Test/index.test.ts
@@ -1,33 +1,46 @@
-import { expect } from 'chai';
-import { Column, LoadFile } from '../index';
+import { expect } from "chai";
+import { Column, LoadFile } from "../index";
 
-@LoadFile('config/test1')
+@LoadFile("config/test1")
 export class INITest1 {
-	@Column()
-	static NAME: string;
+  @Column()
+  static NAME: string;
 
-	@Column()
-	static MATH_SCORE: number;
+  @Column()
+  static MATH_SCORE: number;
 }
 
-@LoadFile('config/test2')
+@LoadFile("config/test2")
 export class INITest2 {
-	@Column()
-	static NAME: string;
+  @Column()
+  static NAME: string;
 
-	@Column()
-	static SCIENCE_SCORE: number;
+  @Column()
+  static SCIENCE_SCORE: number;
 }
 
+@LoadFile("config/test3.env")
+export class INITest3 {
+  @Column()
+  static NAME: string;
 
-describe('Environment Value', () => {
-	it('01_test1.ini', () => {
-		expect(INITest1.NAME).to.be.equal('rhea');
-		expect(INITest1.MATH_SCORE).to.be.equal(0);
-	});
+  @Column()
+  static MATH_SCORE: number;
+}
 
-	it('02_test2.ini', () => {
-		expect(INITest2.NAME).to.be.equal('hades');
-		expect(INITest2.SCIENCE_SCORE).to.be.equal(95);
-	});
+describe("Environment Value", () => {
+  it("01_test1.ini", () => {
+    expect(INITest1.NAME).to.be.equal("rhea");
+    expect(INITest1.MATH_SCORE).to.be.equal(0);
+  });
+
+  it("02_test2.ini", () => {
+    expect(INITest2.NAME).to.be.equal("hades");
+    expect(INITest2.SCIENCE_SCORE).to.be.equal(95);
+  });
+
+  it("03_test3.env", () => {
+    expect(INITest3.NAME).to.be.equal("rhea-so");
+    expect(INITest3.MATH_SCORE).to.be.equal(10);
+  });
 });
